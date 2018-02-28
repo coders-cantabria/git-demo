82c03fbeb3e2c16f593e07e2f496214df1ec4b2a
Jon Surrell <jon.surrell@automattic.com>
2018-02-28 18:14:43 +0100

    Prep solution

---
+++ multiplos.py
@@ -0,0 +1,12 @@
+# Enumerando los números naturales por debajo de 10 que son múltiplos de 3 o 5,
+# obtenemos 3, 5, 6 y 9. La suma de éstos es 23.
+#
+# Encuentra la suma de todos los múltiplos de 3 o 5 por debajo de 1000.
+
+
+def main():
+    print "Unimplemented"
+    pass
+
+if '__main__' == __name__:
+    main()
